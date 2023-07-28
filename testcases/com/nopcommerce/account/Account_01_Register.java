cbf130711d7f8b11d299127aba2ec23de38c8824
lap14153 <lap14153@localhost>
2023-07-28 18:09:40 +0700

    Create project Framework test

---
+++ testcases/com/nopcommerce/account/Account_01_Register.java
@@ -0,0 +1,21 @@
+package com.nopcommerce.account;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Account_01_Register {
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@Test
+	public void TC_01() {
+		//test
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
